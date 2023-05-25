8c21e49889fade36a09eccf47915d591baa814db
Danny Kettle <danny.kettle@dkwebservices.co.uk>
2023-05-26 00:05:52 +0100

    Akamai

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    unoptimized: true,
+    loader: "akamai",
+    path: "",
   },
   basePath: "/EcommerceSEO",
   assetPrefix: "/EcommerceSEO",
