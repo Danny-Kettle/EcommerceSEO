bd4e55a0edccc07d90bcdc68afe676d5b9304e84
Danny Kettle <danny.kettle@dkwebservices.co.uk>
2023-05-26 14:41:59 +0100

    Trailing Slash

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,21 @@
 import { SessionProvider } from "next-auth/react";
 import { CartProvider } from "@/components/CartContext";
 import { ProductProvider } from "@/components/ProductContext";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 import "@/styles/globals.css";
+function RedirectToTrailingSlash() {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!router.asPath.endsWith("/")) {
+      router.push(router.asPath + "/", undefined, { shallow: true });
+    }
+  }, [router]);
+
+  return null;
+}
 
 function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
@@ -10,6 +23,7 @@ function App({ Component, pageProps: { session, ...pageProps } }) {
       <ProductProvider>
         <CartProvider>
           <Component {...pageProps} />
+          <RedirectToTrailingSlash />
         </CartProvider>
       </ProductProvider>
     </SessionProvider>
